1d8ee2dd80525bd5deefa30971b83142b9b70fb5
EmeV <v_emese_z@yahoo.com>
2022-01-31 15:24:10 +0200

    namespace

---
+++ src/components/project-item.ts
@@ -0,0 +1,50 @@
+/// <reference path="base-components.ts" />
+
+namespace App{
+    
+// ProjectItem Class
+export class ProjectItem
+extends Component<HTMLUListElement, HTMLLIElement>
+implements Draggable
+{
+private project: Project;
+
+get persons() {
+  if (this.project.people === 1) {
+    return "1 person";
+  }
+  return `${this.project.people}` + " people";
+}
+
+constructor(hostId: string, project: Project) {
+  super("single-project", hostId, false, project.id);
+  this.project = project;
+
+  this.configure();
+  this.renderContent();
+}
+
+@autobind
+dragStartHandler(event: DragEvent): void {
+  //add data we want to transfer
+  //how can dataTransfer be null? it can be used in dragEndas well, where we might not have the data anymore
+  event.dataTransfer!.setData("text/plain", this.project.id); //the first param is format, which is predefines list of possible formats
+  event.dataTransfer!.effectAllowed = "move"; //change icon of cursor
+}
+
+dragEndHandler(_t: DragEvent) {
+  console.log("DragEnded");
+}
+
+configure() {
+  this.element.addEventListener("dragstart", this.dragStartHandler); //could also do bind()
+  this.element.addEventListener("dragend", this.dragEndHandler);
+}
+
+renderContent() {
+  this.element.querySelector("h2")!.textContent = this.project.title;
+  this.element.querySelector("h3")!.textContent = this.persons;
+  this.element.querySelector("p")!.textContent = this.project.description;
+}
+}
+}
---
+++ src/models/drag-and-drop.ts
@@ -0,0 +1,13 @@
+//Drag and Drop Interfaces
+namespace App {
+  export interface Draggable {
+    dragStartHandler(event: DragEvent): void; //listener
+    dragEndHandler(event: DragEvent): void; //listener
+  }
+
+  export interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void; //react to the drop itself -> use to update data here
+    dragLeaveHandler(event: DragEvent): void; //if we want to give visual feedback, or revert visual handler, if drag was cancelled
+  }
+}
